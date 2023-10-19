ef52f6a909cf4ca336703502aea1dd62baf6bef9
Ayesh Isuru <121268598+baayesh@users.noreply.github.com>
2023-10-19 15:21:07 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Host on vercel
         </a>
       </header>
     </div>
