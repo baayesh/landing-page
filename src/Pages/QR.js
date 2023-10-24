0f788900b43dcf2a5923ac614c514a151df75b8b
Ayesh Isuru <121268598+baayesh@users.noreply.github.com>
2023-10-24 08:44:51 +0530

    Change icons

--- src/Pages/QR.js
+++ src/Pages/QR.js
@@ -1,63 +1,56 @@
 import React from "react";
 import "./QR.css";
 import qrImage from "../Assets/qr.png";
-import YMCA from "../Assets/Partners/Partners/Batticaloa/YMCA Logo.jpg";
-import DFCDF from "../Assets/Partners/Partners/Hambantota/DFCDF Logo.jpg";
-import DCHam from "../Assets/Partners/Partners/Hambantota/LOGO DC Ham.jpg";
-import WDF from "../Assets/Partners/Partners/Hambantota/Logo WDF.jpg";
-import BAY from "../Assets/Partners/Partners/Kilinochchi/BAY.Org.jpeg";
-import MSWF from "../Assets/Partners/Partners/Kilinochchi/MSWF.jpg";
-import NGOC from "../Assets/Partners/Partners/Kilinochchi/NGO Consortium.png";
-import SLCDF from "../Assets/SLCDF.png";
-import DWC from "../Assets/DWC.png";
-import Canada from "../Assets/Canada.png";
 import Back from "../Assets/Coorperate Print.png";
+import insta from "../Assets/QR/Instagram.png";
+import fb from "../Assets/QR/Facebook.png";
+import youtube from "../Assets/QR/Youtube.png";
+import tiktok from "../Assets/QR/tiktok.png";
+import ltfb from "../Assets/QR/Lower Thirds/fb.png";
+import ltin from "../Assets/QR/Lower Thirds/In.png";
+import ltyt from "../Assets/QR/Lower Thirds/yt.png";
+import lttik from "../Assets/QR/Lower Thirds/tiktok.png"
 
 function QR() {
   return (
     <div>
       <div className="bluePane">
-        <h2 className="sidecontent">
-          WE ARE LIVE NOW! 
-        </h2>
+        <h2 className="sidecontent">WE ARE LIVE NOW!</h2>
       </div>
       <img src={Back} alt="" className="backimg" />
       {/* Replace Contetnt */}
       <div class="content">
-   
-
-
         <h2 className="tit">FIND US ON</h2>
         <div class="row">
           <div class="column">
-            <img src={qrImage} alt="" />
+            <img src={fb} alt="" />
           </div>
           <div class="column">
-            <img src={qrImage} alt="" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="column">
-            <h3 className="fb">Facebook</h3>
-          </div>
-          <div class="column">
-            <h3 className="insta">Instagram</h3>
+            <img src={insta} alt="" />
           </div>
         </div>
         <div class="row">
           <div class="column">
-            <img src={qrImage} alt="" />
+            <img src={ltfb} alt="" />
           </div>
           <div class="column">
-            <img src={qrImage} alt="" />
+            <img src={ltin} alt="" />
           </div>
         </div>
         <div class="row">
           <div class="column">
-            <h3 className="fb">Youtube</h3>
+            <img src={youtube} alt="" />
           </div>
           <div class="column">
-            <h3 className="insta">LinkedIn</h3>
+            <img src={tiktok} alt="" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="column">
+            <img src={ltyt} alt="" />
+          </div>
+          <div class="column">
+            <img src={lttik} alt="" />
           </div>
         </div>
       </div>
